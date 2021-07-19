ea70c4adffdb5551482d82a74a5e7d91606fad56
CHIENNV3 <nguyenvanchien3@luvina.net>
2021-07-19 11:09:55 +0700

    Init project

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('users', \App\Http\Controllers\Backend\UserController::class);
+    Route::resource('countries', \App\Http\Controllers\Backend\CountryController::class);
+
+    Route::post('users/{user}/change-password', [\App\Http\Controllers\Backend\ChangePasswordController::class, 'change_password'])->name('users.change.password');
+
